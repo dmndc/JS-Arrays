418e6cf70055434b45553dcd4c79dae585de9b4a
Dino Mandic <dinomandic@hotmail.com>
2017-10-10 17:25:49 -0500

    solved all the problems

--- arraysPractice.js
+++ arraysPractice.js
@@ -92,7 +92,20 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 
 
-  //Code Here
+function divider(numbersArray) {
+  var evens = [];
+  var odds = [];
+
+  numbersArray.forEach(function(item) {
+    if (item % 2 === 0) {
+      evens.push(item);
+    } else {
+      odds.push(item);
+    }
+  })
+
+  return [evens, odds];
+}
 
 
 //Next Problem
@@ -106,7 +119,13 @@ var getRandomArbitrary = function() {
 
 // Your job is to write a function named finder that will get a random number (by invoking getRandomArbitrary), then loop through the array (that will be passed in as a parameter) to see if that random number is in the array. If it is, return true, if it's not, return false
 
-  //Code Here
+function finder(array) {
+  var number = getRandomArbitrary();
+  if (array.indexOf(number) !== - 1) {
+    return true;
+  }
+  return false;
+}
 
 
 
@@ -130,7 +149,19 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
   In both the removeItem function and the addItem function, you will also need to check for valid aurguments. Specrunner will try to call your functions without passing in valid aurguments. When this happens, you will need to respond by returning an empty array.
 */
 
-  //Code Here
+function removeItem(myGroceryList, itemToRemove) {
+  myGroceryList.forEach(function(item, index) {
+    if (item === itemToRemove) {
+      myGroceryList.splice(index, 1);
+    }
+  })
+  return myGroceryList;
+}
+
+function addItem(myGroceryList, item) {
+  myGroceryList.push(item);
+  return myGroceryList;
+}
 
 //removeItem(myGroceryList, 'chips') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 //addItem(myGroceryList, 'Jerky') --> ['pizza', 'hotpockets', 'MtnDew', 'corndogs', 'Jerky'];
@@ -143,7 +174,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 
 //Write a function called maker that creates an array, fills that array with numbers from 1 to 215, then returns the array.
 
-  //Code Here
+function maker() {
+  var array = [];
+  for (var i = 1; i <= 215; i++) {
+    array.push(i);
+  }
+  return array;
+}
 
 
 
@@ -155,7 +192,14 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 //array after adding ten to each item in numbers. *Verify your answer is correct. --> [15, 19, 26, 29, 35, 44, 58]
 
   //Code Here
-
+function addTen(numbers) {
+  return numbers.map(function(item) {
+    if (typeof item === 'string') {
+      return parseInt(item, 10) + 10;
+    } 
+    return item + 10;
+  });
+}
 
 
 //Next Problem
@@ -175,7 +219,13 @@ for(var i = 0; i < num2; i++){
 //Above is some code that adds a random number of values to both arr1 and arr2.
 //Write a function called 'longer' that is given arr1 and arr2 as it's only arguments. Return the array which is longest.
 
-  //Code Here
+ function longer(arr1, arr2) {
+   if (arr1.length > arr2.length) {
+     return arr1;
+   } else {
+     return arr2;
+   } 
+ }
 
 
 /*
@@ -188,7 +238,9 @@ Your 'both' function will be given two arguments, arr1 and arr2 (from the previo
 Example: var arr1 = [1,2,3,4]; var arr2 = [2,4,5,6]; newArray // [2,4]
 */
 
-  //Code Here
+function both(arr1, arr2) {
+  return arr1.filter((n) => arr2.includes(n));
+}
 
 
 
@@ -228,13 +280,21 @@ var colt = {
 array with those four objects. After that console.log the length of the Array and make
 sure that it's equal to 4. */
 
-  //Code Here
+devMountainEmployees = [tyler, cahlan, ryan, colt];
+console.log(devMountainEmployees.length);
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
-  //Code Here
+function removeCahlan() {
+  for (var i = devMountainEmployees.length - 1; i >= 0 ; i--) {
+    if (devMountainEmployees[i].name === 'Cahlan') {
+      devMountainEmployees.splice(i, 1);
+    }
+  }
+}
 
+removeCahlan();
 
 
 
@@ -246,10 +306,11 @@ of Data is to have an Array full of objects. */
 
 //Create an empty array called users.
 
-  //Code Here
+const users = [];
 
 /*Now add three user objects to your users array. Each user object should contain the
 following properties. name, email, password, username.*/
+users = [{ name, email, password, username}, { name, email, password, username}, { name, email, password, username}];
 
 //include this as one of the objects in your array.
 var user1 = {
@@ -258,6 +319,8 @@ var user1 = {
     password: 'iLoveJavaScript',
     username: 'infiniateLoop'
 };
+
+users.push(user1);
 
 //Your Code Here
 
@@ -269,6 +332,12 @@ and those objects contain properties about the specific person you follow.*/
 objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
 Once you find the particular index he's located in, delete him from the array.*/
 
-  //Code Here
+function deleteTyler() {
+  for (var i = users.length - 1; i >= 0; i--) {
+    if (users[i].email === 'tylermcginnis33@gmail.com') {
+      users.splice(i, 1);
+    }
+  }
+}
 
 //The activity we just did is very much how data works in 'the real world'.
